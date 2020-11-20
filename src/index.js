8b5418c0f634bd7ac036998ab7b2e29c6183d402
Alexandr Spiridonov <darkvoice1996@gmail.com>
2020-11-20 18:11:24 +0300

    Webpack configed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-
+// Полифил. Должен находиться в самом верху
 import {newFunc} from "./test";
+
+import './test'
 
 import './styles.css'
 
 newFunc()
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const CopyWebpackPlugin = require('copy-webpack-plugin') // Перекидывает файлы в бандл
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // Экспортирует стили в отдельный файл
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin') // Указывается в optimization
 
 module.exports = {
-    mode: "production",
+    mode: "development",
     entry: {
         main: './src/index.js',
     },
     output: {
         filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src') // Сокращение для относительных путей
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all", // Выводит повторяющийся код в отдельные вендорные файлы
+        },
+        minimize: true,
+        minimizer: [
+            new CssMinimizerWebpackPlugin(),
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin(
@@ -19,12 +35,34 @@ module.exports = {
             }
         ),
         new CleanWebpackPlugin(),
+        new CopyWebpackPlugin(
+            { // Перекидывает файлы в бандл
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, "./src/favicon.ico"),
+                        to: path.resolve(__dirname, "./dist")
+                    }
+                ]
+            }),
+        new MiniCssExtractPlugin(
+            {
+                filename: "[name].[contenthash].css",
+            }
+        ),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '', // Без этой строки не работало
+                        }
+                    },
+                    'css-loader',
+                ]
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -33,7 +71,22 @@ module.exports = {
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     }
+    ,
+    devServer: {
+        port: 4200, // Запускается командой webpack solve
+    }
+
 }
